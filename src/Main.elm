eb077a9cff394e51d54d58ad143542d1986b9961
Renaud Pradenc <rpradenc@incwo.com>
2019-12-05 09:56:15 +0100

    Pick a random word from a list and show it.

--- src/Main.elm
+++ src/Main.elm
@@ -3,48 +3,83 @@ import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 import List exposing (map)
+import Random
+import Random.List
 
 -- MAIN
 
 main = 
-    Browser.sandbox {
-        init = init,
-        update = update,
-        view = view
+    Browser.element {
+        init = init
+    ,   update = update
+    ,   subscriptions = subscriptions
+    ,   view = view
     }
 
 -- MODEL
 
-type alias Model = 
-    List String
+type Model = 
+    Starting
+    | Guessing String
 
-init : Model
-init = 
-    words
+init : () -> ( Model, Cmd Msg )
+init _ = 
+    ( Starting
+    , Cmd.none
+    )
 
 
 words : List String
 words = 
     ["absolute", "behemoth", "cardinal", "destructive", "escape", "follow", "generate", "horny", "idiomatic", "jellyfish", "kettle", "species", "regretable", "trouser", "wisdom"]
 
-
 -- UPDATE
 
-type Msg = Show
+type Msg = 
+    Generate 
+    | Guess (Maybe String, List String)
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of 
-        Show ->
-            model
+        Generate ->
+            (model
+            , Random.generate Guess (Random.List.choose words)
+            )
 
+        Guess (maybeWord, _) ->
+            ( Guessing (wordOrDefault maybeWord)
+            , Cmd.none
+            )
+
+wordOrDefault : Maybe String -> String
+wordOrDefault maybeWord = 
+    case maybeWord of 
+        Just word ->
+            word
+        Nothing ->
+            "D-Fault"
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
-    div [] 
-        (map stringInDiv model)
+    case model of
+        Starting ->
+            div [] 
+            [ stringInDiv "Welcome to Hangman"
+            , button [ onClick Generate ] [ text "Start" ]
+            ]
+            
+
+        Guessing word ->
+            stringInDiv word
 
 stringInDiv : String -> Html Msg
 stringInDiv str =
